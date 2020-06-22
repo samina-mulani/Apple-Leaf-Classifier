94e59d4a2d04718e26e7d06974c63271ea7787ae
samina-mulani <43271817+samina-mulani@users.noreply.github.com>
2020-06-22 19:59:08 +0530

    FileProvider and runtime permissions added for SDK ver >= 24

--- app/src/main/java/com/example/appleleafclassifier/MainActivity.java
+++ app/src/main/java/com/example/appleleafclassifier/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.appleleafclassifier;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 
 public class MainActivity extends AppCompatActivity {
-    private static final int PICK_IMAGE = 100;
-    private static final int CAMERA = 200;
-    Uri mUri;
-    Uri imageUri;
+    public static final int PERMISSION_REQUEST_CODE = 114; // request code for Camera and External Storage permission
+    private static final int CAMERA_REQUEST_CODE = 133; // request code for capture image
+    private static final int PICK_IMAGE = 100; // request code for gallery
+    Uri mUri; // image URI for image taken through Camera
+    Uri imageUri; // image URI for image chosen through Gallery
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
@@ -47,22 +66,163 @@ public class MainActivity extends AppCompatActivity {
             imageUri = data.getData();
             Intent intent = new Intent(this, DisplayResult.class);
             intent.setData(imageUri);
-            startActivity(intent);
-        } else if (resultCode == RESULT_OK && requestCode == CAMERA) {
+            startActivity(intent); // Display Result Activity initiated
+        } else if (resultCode == RESULT_OK && requestCode == CAMERA_REQUEST_CODE) {
             Intent intent = new Intent(this, DisplayResult.class);
             intent.setData(mUri);
-            startActivity(intent);
+            startActivity(intent); // Display Result Activity initiated
         }
     }
 
-    public void goToCamera(View view) {
+    public void captureImage() {
         Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        mUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "pic_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
-        cam.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+        mUri = getOutputMediaFileUri(this);
         if (cam.resolveActivity((getPackageManager())) != null) {
-            startActivityForResult(cam, CAMERA);
+            cam.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+            startActivityForResult(cam, CAMERA_REQUEST_CODE);
         } else {
             Log.d("Camera implicit intent", "Can't handle this intent");
         }
     }
+
+    public Uri getOutputMediaFileUri(Context context) {
+        File mediaStorageDir = new File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Camera");
+        //If File is not present create directory
+        if (!mediaStorageDir.exists()) {
+            if (mediaStorageDir.mkdir())
+                Log.e("Create Directory", "Main Directory Created : " + mediaStorageDir);
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());//Get Current timestamp
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");//create image path with system mill and image format
+        return FileProvider.getUriForFile(MainActivity.this,
+                BuildConfig.APPLICATION_ID + ".provider",
+                mediaFile);
+        //return Uri.fromFile(mediaFile);
+    }
+
+    /*  Check both permissions  */
+    private boolean checkPermission() {
+        ArrayList<String> permissions = new ArrayList<>();
+        for (String permission : getAllPermissions()) {
+            int result = checkPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(permission);
+            }
+        }
+        //If both permissions are granted
+        if (permissions.size() == 0)
+            allPermissionGranted();
+        else
+            //if any one of them are not granted then request permission
+            requestPermission(permissions.toArray(new String[permissions.size()]));
+        return true;
+    }
+
+    /*   on both permission granted  */
+    private void allPermissionGranted() {
+        //Initiate capture image method
+        if (isDeviceSupportCamera())
+            captureImage();
+    }
+
+    /*  Request permissions  */
+    private void requestPermission(String[] permissions) {
+        ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+    }
+
+    /*  Permissions string array  */
+    private String[] getAllPermissions() {
+        return new String[]{CAMERA, WRITE_EXTERNAL_STORAGE};
+    }
+
+    /*  Method to check permissions  */
+    public static int checkPermission(final Context context, String permission) {
+        return ContextCompat.checkSelfPermission(context, permission);
+    }
+
+    /*  on Capture image button click check permissions  */
+    public void goToCamera(View view) {
+        checkPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    int counter = 0;//counter to traverse all permissions
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            //show alert dialog if any of the permission denied
+                            showMessageOKCancel(getString(R.string.permission_message),
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                //If user click on OK button check permission again.
+                                                checkPermission();
+                                            }
+                                        }
+                                    }, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Toast.makeText(MainActivity.this, R.string.capture_deny_message, Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                            return;
+                        } else {
+                            counter++;
+                            //If counter is equal to permissions length mean all permission granted.
+                            if (counter == permissions.length)
+                                allPermissionGranted();
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    /*  Alert dialog on permission denied    */
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener, DialogInterface.OnClickListener cancelListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, okListener)
+                .setNegativeButton(android.R.string.cancel, cancelListener)
+                .setCancelable(false)
+                .create()
+                .show();
+    }
+
+    // Checking camera supportability
+    private boolean isDeviceSupportCamera() {
+        if (getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA_ANY))
+            return true;
+        else {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.camera_not_supported), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save file url in bundle as it will be null on screen orientation
+        // changes
+        outState.putParcelable("file_uri", mUri);
+    }
+
+    /*
+     * Here we restore the fileUri again
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // get the file url
+        mUri = savedInstanceState.getParcelable("file_uri");
+    }
+
 }
